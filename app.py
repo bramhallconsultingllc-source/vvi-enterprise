686de4a5510a84d4be0b509a13136bd8753a64d1
Michael Bramhall <bramhallconsultingllc@gmail.com>
2026-02-13 16:12:31 -0700

    Update app.py

--- app.py
+++ app.py
@@ -24,7 +24,6 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-from fpdf import FPDF
 
 # ============================================================
 # Configuration & Settings
